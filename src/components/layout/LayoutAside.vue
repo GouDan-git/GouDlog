7590f14866ebf0276878f87dbe84b66b95e6aa7f
GouDan <1365815077@qq.com>
2022-10-09 10:39:51 +0800

    feat: 完成侧边栏路由功能

--- src/components/layout/LayoutAside.vue
+++ src/components/layout/LayoutAside.vue
@@ -1,11 +1,18 @@
 <template>
-  <el-menu class="el-menu-vertical" :collapse="true">
+  <el-menu
+    class="el-menu-vertical"
+    :collapse="true"
+    :default-active="activeIndex"
+    @select="handleSelect"
+  >
     <el-menu-item
       :index="item.name"
       v-for="(item, index) in asideMenu"
       :key="index"
     >
-      <el-icon><icon-menu /></el-icon>
+      <el-icon>
+        <component :is="item.icon"></component>
+      </el-icon>
       <template #title>{{ item.name }}</template>
     </el-menu-item>
   </el-menu>
@@ -13,17 +20,36 @@
 
 <script lang="ts" setup>
 import { ref, watch } from "vue";
-import { Menu as IconMenu } from "@element-plus/icons-vue";
 import { useStore } from "vuex";
+import { useRouter } from "vue-router";
 
+/**
+ * @description 监听store中asideMenu变化来更新侧边栏
+ */
+const activeIndex = ref("");
 const store = useStore();
 let asideMenu = ref(store.state.menu.asideMenu);
 watch(
   () => store.state.menu.asideMenu,
-  (newVal: Object) => {
+  (newVal: Array<any>) => {
     asideMenu.value = newVal;
+    if (newVal && newVal.length) {
+      activeIndex.value = newVal[0].name;
+    }
   }
 );
+
+/**
+ * @description 侧边栏点击事件
+ * @param key 点击的菜单名称
+ */
+const router = useRouter();
+const handleSelect = (key: Object) => {
+  const items = asideMenu.value.filter((item: any) => {
+    return item.name === key;
+  });
+  router.push(items[0].path);
+};
 </script>
 
 <style lang="scss" scoped>
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import router from "./router";
 import store from "./store";
 import ElementPlus from "element-plus";
 import WujieVue from "wujie-vue3";
+import * as Icons from "@element-plus/icons-vue";
 
 import "element-plus/dist/index.css";
 import "@/assets/main.scss";
@@ -14,5 +15,11 @@ app.use(router);
 app.use(ElementPlus);
 app.use(WujieVue);
 app.use(store);
+/**
+ * @description element-plus 图标导入 注册全局组件
+ */
+Object.keys(Icons).forEach((key) => {
+  app.component(key, Icons[key]);
+});
 
 app.mount("#app");
